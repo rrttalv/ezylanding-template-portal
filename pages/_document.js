2a2866825d4b559f7be1e1ccf775e3bdb2e17b90
Rico Rein Talvar <ricotalvar@gmail.com>
2022-04-27 10:09:10 +0300

    twtr

--- pages/_document.js
+++ pages/_document.js
@@ -10,17 +10,18 @@ const Document = () => {
       <meta name="title" content="EzyLanding | Webpack HTML Templates For Startups" />
       <meta name="description" content="About to get started with your next project? Get a landing page from us! At EzyLanding you will find customizeable HTML templates for startups!"/>
 
-      <meta property="og:type" content="website"/>
-      <meta property="og:url" content="https://ezylanding.com"/>
-      <meta property="og:title" content="EzyLanding | Webpack HTML Templates For Startups" />
-      <meta property="og:description" content="About to get started with your next project? Get a landing page from us! At EzyLanding you will find customizeable HTML templates for startups!" />
-      <meta property="og:image" content="/assets/meta-preview-image.png" />
-
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://ezylanding.com" />
       <meta property="twitter:title" content="EzyLanding | Webpack HTML Templates For Startups" />
       <meta property="twitter:description" content="About to get started with your next project? Get a landing page from us! At EzyLanding you will find customizeable HTML templates for startups!" />
-      <meta property="twitter:image" content="https://ezylanding.com/assets/meta-preview-image.png" />
+      <meta property="twitter:image" content="https://ezylanding-user-assets.s3.amazonaws.com/media/meta-preview-image.png" />
+
+      <meta property="og:type" content="website"/>
+      <meta property="og:url" content="https://ezylanding.com"/>
+      <meta property="og:title" content="EzyLanding | Webpack HTML Templates For Startups" />
+      <meta property="og:description" content="About to get started with your next project? Get a landing page from us! At EzyLanding you will find customizeable HTML templates for startups!" />
+      <meta property="og:image" content="https://ezylanding-user-assets.s3.amazonaws.com/media/meta-preview-image.png" />
+      
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&family=Roboto+Mono&display=swap" rel="stylesheet" />
